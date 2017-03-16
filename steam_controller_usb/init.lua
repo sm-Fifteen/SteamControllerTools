5f756e534dab3f33c22068e23866e0ccec94328d
zvodd <zv.odd.101@gmail.com>
2017-03-17 02:50:16 +1100

    fix load order from issue #4

--- steam_controller_usb/init.lua
+++ steam_controller_usb/init.lua
@@ -1,8 +1,1 @@
 print( (require 'debug').getinfo(1).source )
-
-require("bit")
-
--- Dissector Table for steam controller control packets
-scPacketTable = DissectorTable.new("sc_packet.msgType", "Steam Controller Packet", ftypes.UINT8, base.HEX)
-scConfigTable = DissectorTable.new("sc_config.configType", "Steam Controller Config", ftypes.UINT8, base.HEX) -- 0x87
-scUpdateTable = DissectorTable.new("sc_update.stateType", "Steam Controller state update", ftypes.UINT8, base.HEX) --0x01
